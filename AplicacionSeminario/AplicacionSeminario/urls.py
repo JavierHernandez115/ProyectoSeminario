0950568892e9aa4c62f0c74ab6d6fbc8e47b9cec
JavierHernandez115 <hernandezmartinezjavier115@gmail.com>
2024-09-04 22:45:56 -0600

    Arreglando el enrutamiento de rutas

--- AplicacionSeminario/AplicacionSeminario/urls.py
+++ AplicacionSeminario/AplicacionSeminario/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 
@@ -39,6 +39,8 @@ router.register(r'categorias', CategoriaViewSet)
 router.register(r'subcategorias', SubcategoriaViewSet)
 router.register(r'datos_bancarios', DatosBancariosViewSet)
 router.register(r'gastos', GastosViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
